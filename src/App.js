d4d5266e2508ac2933ed6ae9ef5fe636b1e0d583
Aida <aidachernyavskaya@gmail.com>
2023-04-03 13:23:23 +0300

    custom hook useFetching added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useMemo, useRef, useState} from "react";
+import React, {useEffect, useState} from "react";
 import "./styles/App.css";
 import PostList from "./components/PostList";
 import {MyButton} from "./components/UI/button/MyButton";
@@ -6,16 +6,19 @@ import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import {usePosts} from "./hooks/usePosts";
-import axios from "axios";
 import PostService from "./API/PostService";
 import Loader from "./components/UI/Loader/Loader";
+import {useFetching} from "./hooks/useFetching";
 
 function App() {
     const [posts, setPosts] = useState([]);
     const [filter, setFilter] = useState({sort: '', query: ''});
     const [modal, setModal] = useState(false);
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-    const [isPostsLoading, setIsPostsLoading] = useState(false);
+    const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+        const posts = await PostService.getAll();
+        setPosts(posts);
+    });
 
     useEffect (() => {
         fetchPosts();
@@ -24,15 +27,6 @@ function App() {
     const createPost = (newPost) => {
         setPosts([...posts, newPost]);
         setModal(false);
-    }
-
-    async function fetchPosts() {
-        setIsPostsLoading(true);
-        setTimeout(async () => {
-            const posts = await PostService.getAll();
-            setPosts(posts);
-            setIsPostsLoading(false);
-        }, 1000)
     }
 
     const removePost = (post) => {
@@ -47,6 +41,7 @@ function App() {
             </MyModal>
             <hr style={{margin: '15px 0'}}/>
             <PostFilter filter={filter} setFilter={setFilter}/>
+            {postError && <h1>Произошла ошибка ${postError}</h1>}
             {isPostsLoading
                 ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div>
                 : <PostList remove={removePost} posts={sortedAndSearchedPosts} title={'Посты про JS'}/>
