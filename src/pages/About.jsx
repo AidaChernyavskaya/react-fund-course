fa48a85515fca865badf1d20a0e462926c856a92
Aida <aidachernyavskaya@gmail.com>
2023-04-04 14:42:01 +0300

    page navigation with react router added

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <h1 style={{marginTop: 50, textAlign: 'center'}}>
+            Это приложение создано в качестве практики по обучающему ролику
+        </h1>
+    );
+};
+
+export default About;
