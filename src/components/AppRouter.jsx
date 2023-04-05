879909e4890674c80b85b4346bbd0363d51ced6f
Aida <aidachernyavskaya@gmail.com>
2023-04-05 15:43:03 +0300

    useContext hook for auth added

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Navigate, Route, Routes} from "react-router-dom";
 import {publicRoutes, privateRoutes} from "./router";
+import {AuthContext} from "../context";
+import Loader from "./UI/Loader/Loader";
 
 const AppRouter = () => {
-    const isAuth = true;
+    const {isAuth, isLoading} = useContext(AuthContext);
+
+    if(isLoading){
+        return <Loader/>
+    }
 
     return (
         isAuth
             ?   <Routes>
                     {privateRoutes.map(route =>
-                        <Route path={route.path} element={route.element}></Route>
+                        <Route path={route.path} element={route.element} key={route.path}></Route>
                     )}
                     <Route path="/*" element={<Navigate replace to="/posts" />} />
                 </Routes>
             :   <Routes>
                     {publicRoutes.map(route =>
-                        <Route path={route.path} element={route.element}></Route>
+                        <Route path={route.path} element={route.element} key={route.path}></Route>
                     )}
                     <Route path="/*" element={<Navigate replace to="/login" />} />
                 </Routes>
--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -34,7 +34,7 @@ const PostIdPage = () => {
                 ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div>
                 : <div>
                     {comments.map(com =>
-                        <div style={{marginTop: 15}}>
+                        <div style={{marginTop: 15}} key={com.id}>
                             <h5>{com.email}</h5>
                             <div>{com.body}</div>
                         </div>
